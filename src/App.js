4104a6ec5770f2d43a517fffcf86cd3ec58f0f06
Mehtap <kayhanmehtap10@gmail.com>
2022-04-21 20:06:54 +0200

    Searchbar is created and input value is given as prop to Article

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import './App.css';
 import Articles from "./Components/Articles";
+import Searchbar from "./Components/Searchbar";
 
 function App() {
   
   return  ( 
-      
-        <div className = "App" >
-          <Articles/>
+        <div>
+          <Searchbar/>
+          
         </div>
       )
     
---
+++ src/Components/Searchbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Articles from "./Articles";
+import { useState, useEffect } from "react";
+
+
+
+const Searchbar = () => {
+
+    const[search, setSearch] = useState("");
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearch(e.target.firstChild.value);
+        console.log(e.target.firstChild.value);
+        console.log(`Ich bin ein StateWert, ${search}`)
+        }
+    
+    return(
+        <div id="form-container">
+            
+            <form onSubmit={handleSearch}>
+                <input type="text" id="text-field" placeholder="Search..."></input>
+                <button type="submit">Search</button>
+            </form>
+            
+
+            <div class="block">
+                <li id="search-list"></li>
+                
+            </div>
+            <Articles query={search}/>
+        </div>
+            
+
+    )
+}
+
+export default Searchbar;
